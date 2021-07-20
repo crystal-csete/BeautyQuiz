ea664ef721bc65b39d2d1c0e025ae17a1cf697b9
Crystal Csete <jeannecc13@gmail.com>
2021-07-20 16:45:36 -0600

    Adds styles to Navigation and Home files

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,18 +1,28 @@
 /** @format */
 
 import React from "react";
-import { Link } from "react-router-dom";
-import Home from "./Home";
-import BeautyQuiz from "./BeautyQuiz";
 
 const Navigation = () => {
   return (
-    <nav>
-      <ul>
-        <Link to='/beautyquiz'>Beauty Quiz</Link>
-
-        <Link to='/'>Home</Link>
-      </ul>
+    <nav className='navbar navbar-light bg-light'>
+      <div className='container-fluid'>
+        <a className='navbar-brand' href='/'>
+          Home
+        </a>
+        <a className='navbar-brand' href='/beautyquiz'>
+          Beauty Quiz
+        </a>
+        <form className='d-flex'>
+          <input
+            className='form-control me-2'
+            type='search'
+            placeholder='Search'
+            aria-label='Search'></input>
+          <button className='btn btn-outline-success' type='submit'>
+            Search
+          </button>
+        </form>
+      </div>
     </nav>
   );
 };
